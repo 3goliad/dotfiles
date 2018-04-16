00d1a9cec62b5498d652832538244b0e17bdacad
Javier Maldonado <3goliad@gmail.com>
2018-04-16 09:59:18 -0400

    dir now installs, uninstalls, verifies

--- lib/dir.rkt
+++ lib/dir.rkt
@@ -11,18 +11,24 @@
          (init location)
          (super-new)
          (inherit-field name)
-         (define to location)
-         (define from 
+         (define to
+           (expand-user-path location))
+         (define from
            (expand-user-path (string-append "~/c/" name)))
          (define/override (installed?)
            (directory-exists? to))
          (define/override (verified?)
-           (= (file-or-directory-identity from)
-              (file-or-directory-identity to)))
+           (and (installed?)
+                (= (file-or-directory-identity from)
+                   (file-or-directory-identity to))))
          (define/override (install)
            (make-file-or-directory-link from to))
          (define/override (uninstall)
-           (delete-directory to))))
+           (cond
+            [(or (file-exists? to) (link-exists? to))
+             (delete-file to)]
+            [(directory-exists? to)
+             (delete-directory to)]))))
 
 (define (dir name location)
   (new dir% [name name] [location location]))
--- test/dir-test.rkt
+++ test/dir-test.rkt
@@ -5,12 +5,49 @@
          racket/class
          "../lib/dir.rkt")
 
+(define d (dir "test" ".symlink-test"))
+(define p (expand-user-path  "~/c/.symlink-test"))
+
 (test-case
-  "symlinks a directory in the config repo to the homedir"
-  (define d (dir "test" ".symlink-test"))
-  (send d run)
-  (check-pred link-exists? "~/c/.symlink-test")
-  (check-pred file-exists? "~/c/.symlink-test")
-  (check-true (send d installed?))
-  (check-true (send d verified?))
-  (delete-file "~/c/.symlink-test"))
+ "symlinks a directory in the config repo to the homedir"
+ (send d run)
+ (check-pred link-exists? p)
+ (check-true (send d installed?))
+ (check-true (send d verified?))
+ (delete-file p))
+
+(test-case
+ "deletes the symlink to the directory when uninstalled"
+ (send d run)
+ (send d uninstall)
+ (check-false (link-exists? p))
+ (check-false (send d installed?))
+ (check-false (send d verified?)))
+
+(test-case
+ "never needs to update"
+ (check-false (send d needs-update?))
+ (send d run)
+ (check-false (send d needs-update?))
+ (send d uninstall)
+ (check-false (send d needs-update?)))
+
+(test-case
+ "replaces the symlink if deleted"
+ (send d run)
+ (delete-file p)
+ (check-false (send d installed?))
+ (send d run)
+ (check-true (send d installed?))
+ (delete-file p))
+
+(test-case
+ "fixes a mangled symlink"
+ (send d run)
+ (delete-file p)
+ (make-directory p)
+ (check-false (send d verified?))
+ (check-true (send d installed?))
+ (send d run)
+ (check-true (send d verified?))
+ (send d uninstall))
