f35138d1602d2c8d91a7912824580111b1c0c487
Javier Maldonado <3goliad@gmail.com>
2018-04-16 20:29:00 -0400

    moving to manifest to centralize config mods

--- manifest.rkt
+++ manifest.rkt
@@ -1,20 +1,22 @@
                                         ; -*-scheme-*-
 #lang racket/base
 
-(require "../lib/dir.rkt")
-(require "../lib/git.rkt")
-(require "../lib/pacman.rkt")
+(require "lib/dir.rkt")
+(require "lib/git.rkt")
+(require "lib/pacman.rkt")
 
-(provide emacs)
+(provide mods)
 
-(define (emacs)
+(define emacs
   (list
-   (dir "doom.d" "~/.doom.d")
    (pacman "emacs")
+   (dir "doom.d" "~/.doom.d")
    (git-repo "doom-emacs"
              "https://github.com/hlissner/doom-emacs"
              "~/.emacs.d"
-             #:depends '("doom.d")
              #:update '("make upgrade" "make update")
              #:install "make install"
              #:post-change "make compile")))
+
+(define mods
+  emacs)
