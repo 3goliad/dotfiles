3e5321da64950c97c44a787485cfc930ff65122e
3goliad <3goliad@gmail.com>
2018-04-23 17:41:45 -0400

    fleshing out a configurator for pacman

--- lib/pacman.rkt
+++ lib/pacman.rkt
@@ -1,14 +1,26 @@
                                         ; -*-scheme-*-
 #lang racket/base
 
-(require racket/class)
+(require racket/class
+         racket/system)
 (require "config.rkt")
 
 (provide pacman)
 
 (define pacman%
   (class config%
-         (super-new)))
+         (super-new)
+         (inherit-field name)
+         (define/private (pacman-op op [sudo #f])
+           (system (string-append (if sudo "sudo " "") "pacman " op " " name)))
+         (define/override (installed?)
+           (pacman-op "-Q"))
+         (define/override (install)
+           (pacman-op "-S" #t))
+         (define/override (uninstall)
+           (pacman-op "-Rnss" #t))
+         (define/override (needs-update?))
+         (define/override (update))))
 
 (define (pacman name)
   (make-object pacman% name))
--- test/pacman-test.rkt
+++ test/pacman-test.rkt
@@ -2,9 +2,34 @@
 #lang racket/base
 
 (require rackunit racket/class "../lib/pacman.rkt")
+(require racket/system)
 
 (define mod (pacman "redis"))
 
+
 (test-case
  "detects whether the package is installed"
- (check-false (send d installed?)))
+ (system "sudo pacman --noconfirm -R redis")
+ (check-false (send mod installed?))
+ (system "sudo pacman --noconfirm -S redis")
+ (check-true (send mod installed?)))
+
+(system "sudo pacman --noconfirm -R redis")
+
+(test-case
+ "installs a package"
+ (system "sudo pacman --noconfirm -R redis")
+ (send mod install)
+ (check-true (system "pacman -Q redis")))
+
+(system "sudo pacman --noconfirm -R redis")
+
+(test-case
+ "uninstalls a package"
+ (system "sudo pacman --noconfirm -S redis")
+ (send mod uninstall)
+ (check-false (system "pacman -Q redis")))
+
+(test-case
+ "updates a package"
+ )
