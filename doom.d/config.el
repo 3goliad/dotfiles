bb55c5eb0541cf5b944cd9673f9a5768a1e9dde2
Javier Maldonado <3goliad@gmail.com>
2018-04-27 22:32:32 -0400

    changing emacs font
    I don't have a way to install Iosevka everywhere atm

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,9 +1,9 @@
 ;;;  -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Iosevka Term Slab"
+(setq doom-font (font-spec :family "Fira Mono"
                            :size 18))
 
-(setq doom-big-font (font-spec :family "Iosevka Term Slab"
+(setq doom-big-font (font-spec :family "Fira Mono"
                            :size 30))
 (after! js2-mode
   (setq js-indent-level 2))
