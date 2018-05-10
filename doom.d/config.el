772803f30982934fbdb151fed8eee2a598ceb3a2
3goliad <3goliad@gmail.com>
2018-05-10 14:32:28 -0400

    fix fonts in emacs and alacritty

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,9 +1,17 @@
 ;;;  -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Fira Mono"
-                           :size 18))
+(if (string= system-name "nuc-if-u-buc")
+    (setq doom-font (font-spec :family "Iosevka Term Slab"
+                               :size 18))
+  (setq doom-font (font-spec :family "Fira Mono"
+                             :size 18)))
 
-(setq doom-big-font (font-spec :family "Fira Mono"
-                           :size 30))
+
+(if (string= system-name "nuc-if-u-buc")
+    (setq doom-big-font (font-spec :family "Iosevka Term Slab"
+                                   :size 30))
+  (setq doom-big-font (font-spec :family "Fira Mono"
+                                 :size 30)))
+
 (after! js2-mode
   (setq js-indent-level 2))
