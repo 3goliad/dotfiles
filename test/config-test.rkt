1d4597208aeef7a75c2e84ff7432b8e72db52c9c
3goliad <3goliad@gmail.com>
2018-05-31 19:30:15 -0400

    removing garbage

--- test/config-test.rkt
+++
@@ -1,6 +0,0 @@
-                                        ; -*-scheme-*-
-#lang racket/base
-
-(require rackunit racket/class "../lib/config.rkt")
-
-(check-equal? (updates-show '()) '("No configurations are registered!") "Error message for no configs")
