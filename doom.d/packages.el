bf57384b36d5332c3da75d9ea290b079f192a18f
lies_amouldering <3goliad@gmail.com>
2021-04-02 13:32:15 -0400

    add anaconda

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -2,3 +2,6 @@
 
 (package! ws-butler)
 (package! dockerfile-mode)
+(package! anaconda-mode
+  :recipe (:host github :repo "pythonic-emacs/anaconda-mode")
+  :pin "80afec20f91f13614647b192522fff460505db6f")
