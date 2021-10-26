1aa0b306fbd86304eb2d3cfd0f1b845352113207
lies_amouldering <3goliad@gmail.com>
2021-10-26 12:45:21 -0400

    adding custom vars for doom

---
+++ doom.d/custom.el
@@ -0,0 +1,20 @@
+;;; custom.el --- serlisto vars                      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021
+
+;; Author:  <jam@slimjim>
+;; Keywords:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((cider-ns-refresh-after-fn . "integrant.repl/resume")
+     (cider-ns-refresh-before-fn . "integrant.repl/suspend"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
