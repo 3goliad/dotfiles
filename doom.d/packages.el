e6a65bf844d02ce23777f592f8aa4c9d8192b4d1
lies_amouldering <3goliad@gmail.com>
2021-10-26 12:44:45 -0400

    updating doom

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -2,6 +2,3 @@
 
 (package! ws-butler)
 (package! dockerfile-mode)
-(package! anaconda-mode
-  :recipe (:host github :repo "pythonic-emacs/anaconda-mode")
-  :pin "80afec20f91f13614647b192522fff460505db6f")
