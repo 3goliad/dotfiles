e50072ea677774772bc0efc1ecec204a858af033
Javier Maldonado <3goliad@gmail.com>
2024-03-19 09:49:35 -0400

    adjustments to autocomplete

--- personal/dot-doom.d/config.el
+++ personal/dot-doom.d/config.el
@@ -9,31 +9,36 @@
 (setq org-directory "~/d/notes")
 (setq deft-directory org-directory)
 
-(after! hideshow
-  (add-to-list 'hs-special-modes-alist
-               `(ruby-mode
-                 ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
-                 ,(rx (or "}" "]" "end"))                       ; Block end
-                 ,(rx (or "#" "=begin"))                        ; Comment start
-                 ruby-forward-sexp nil)))
+;; (after! ruby)
+;; (require 'company)
+;; (setq company-idle-delay 0.2
+;;       company-minimum-prefix-length 3)
+;; (setq-default flycheck-disabled-checkers '(ruby-reek ruby-rubylint))
 
-(after! js2-mode
-  (setq js-indent-level 2)
-  (require 'company)
-  (setq company-idle-delay 0.2
-        company-minimum-prefix-length 3))
 
-(after! ruby-mode
-  (require 'company)
-  (setq company-idle-delay 0.2
-        company-minimum-prefix-length 3)
-  (setq-default flycheck-disabled-checkers '(ruby-rubocop ruby-reek ruby-rubylint)))
+(add-hook! ruby-mode
+  (setq ruby-block-indent nil))
 
-(after! typescript-mode
-  (setq typescript-indent-level 2)
-  (require 'company)
-  (setq company-idle-delay 0.2
-        company-minimum-prefix-length 3))
 
-(after! ws-butler
-  (ws-butler-global-mode t))
+;; (after! hideshow
+;;   (add-to-list 'hs-special-modes-alist
+;;                `(ruby-mode
+;;                  ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
+;;                  ,(rx (or "}" "]" "end"))                       ; Block end
+;;                  ,(rx (or "#" "=begin"))                        ; Comment start
+;;                  ruby-forward-sexp nil)))
+
+;; (after! js2-mode
+;;   (setq js-indent-level 2)
+;;   (require 'company)
+;;   (setq company-idle-delay 0.2
+;;         company-minimum-prefix-length 3))
+
+;; (after! typescript-mode
+;;   (setq typescript-indent-level 2)
+;;   (require 'company)
+;;   (setq company-idle-delay 0.2
+;;         company-minimum-prefix-length 3))
+
+;; (after! ws-butler
+;;   (ws-butler-global-mode t))
