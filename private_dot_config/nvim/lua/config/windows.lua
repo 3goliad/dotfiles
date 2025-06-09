cd5e28708c69d59670b53a6af60c726467420056
Javier Maldonado <3goliad@gmail.com>
2025-06-09 17:22:07 -0400

    formatting

--- private_dot_config/nvim/lua/config/windows.lua
+++ private_dot_config/nvim/lua/config/windows.lua
@@ -1,18 +1,18 @@
 -- Splitting windows
-vim.keymap.set("n", "<leader>ws", "<C-w>v", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>wS", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split horizontally" })
 
 -- Closing windows
-vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = "Close the current window" })
+vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = "Close window" })
 
 -- Switching windows
-vim.keymap.set("n", "<leader>wh", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<leader>wl", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<leader>wj", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<leader>wk", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>wh", "<C-w><C-h>", { desc = "Focus left window" })
+vim.keymap.set("n", "<leader>wl", "<C-w><C-l>", { desc = "Focus right window" })
+vim.keymap.set("n", "<leader>wj", "<C-w><C-j>", { desc = "Focus lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-w><C-k>", { desc = "Focus upper window" })
 
 -- Moving windows
-vim.keymap.set("n", "<leader>wH", "<C-w>H", { desc = "Move window to far left" })
-vim.keymap.set("n", "<leader>wL", "<C-w>L", { desc = "Move window to far right" })
-vim.keymap.set("n", "<leader>wJ", "<C-w>J", { desc = "Move window to far bottom" })
+vim.keymap.set("n", "<leader>wH", "<C-w>H", { desc = "Move window far left" })
+vim.keymap.set("n", "<leader>wL", "<C-w>L", { desc = "Move window far right" })
+vim.keymap.set("n", "<leader>wJ", "<C-w>J", { desc = "Move window far bottom" })
 vim.keymap.set("n", "<leader>wK", "<C-w>K", { desc = "Move window to far top" })
---
+++ private_dot_config/nvim/lua/plugins/format.lua
@@ -0,0 +1,56 @@
+-- StyLua
+local styluapath = vim.fs.normalize("~/.local/bin/stylua")
+if not (vim.uv or vim.loop).fs_stat(styluapath) then
+  local out = vim.fn.system({
+    "cargo",
+    "install",
+    "--root=~/.local",
+    "stylua",
+  })
+  if vim.v.shell_error ~= 0 then
+    error("Error installing StyLua:\n" .. out)
+  end
+end
+
+return {
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>bf",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
+}
