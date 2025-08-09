1a3740f36120c029a376ffd06a7c8964aa205d70
Javier Maldonado <3goliad@gmail.com>
2025-08-09 11:35:08 -0400

    set up ruff as lsp

--- private_dot_config/nvim/lua/config/lang/python.lua
+++ private_dot_config/nvim/lua/config/lang/python.lua
@@ -1,1 +1,16 @@
-require("conform").formatters_by_ft.python = { "ruff" }
+Brown:add_dep("ruff")
+
+vim.lsp.config("ruff", {
+  cmd = { "ruff", "server" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+  settings = {},
+})
+
+vim.lsp.enable("ruff")
+
+require("conform").formatters_by_ft.python = {
+  "ruff_fix",
+  "ruff_format",
+  "ruff_organize_imports",
+}
