f85474359f97296508ff0ff90796b63ff88a1b96
Javier Maldonado <3goliad@gmail.com>
2026-02-17 15:45:33 -0500

    remove lisp repl functionality

--- private_dot_config/nvim/lua/plugins/conjure.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Olical/conjure",
-  ft = { "scheme", "clojure", "lisp", "racket" },
-  lazy = true,
-  init = function()
-    vim.g["conjure#filetypes"] = { "scheme", "clojure", "lisp", "racket" }
-    vim.g["conjure#filetype#scheme"] = "conjure.client.guile.socket"
-  end,
-}
