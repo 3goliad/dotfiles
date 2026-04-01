2d55a5876e64419b9bc15cea9a435981d063ada8
Javier Maldonado <3goliad@gmail.com>
2026-04-01 10:58:57 -0400

    add some new mappings

--- private_dot_config/nvim/lua/plugins/codediff.lua
+++ private_dot_config/nvim/lua/plugins/codediff.lua
@@ -9,5 +9,12 @@ return {
         height = 14,
       },
     })
+
+    vim.keymap.set(
+      "n",
+      "<leader>gD",
+      "<cmd>CodeDiff<CR>",
+      { desc = "Code[D]iff" }
+    )
   end,
 }
--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -60,6 +60,7 @@ return {
           desc = "[S]earch this [B]uffer",
         }
       )
+      Brown.keymaps.g:add({ "s", tb.builtin, desc = "Search Git [S]tatus" })
       Brown.keymaps.f:add({
         ".",
         tb.oldfiles,
