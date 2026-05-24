89628137a91fd04aef5086d67edd218cb508a070
Javier Maldonado <3goliad@gmail.com>
2026-05-24 10:32:43 -0400

    set up mini.highlighting

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -48,5 +48,26 @@ return {
     require("mini.surround").setup()
 
     require("mini.splitjoin").setup()
+
+    require("mini.hipatterns").setup({
+      highlighters = {
+        fixme = {
+          pattern = "%f[%w]()FIXME()%f[%W]",
+          group = "MiniHipatternsFixme",
+        },
+        hack = {
+          pattern = "%f[%w]()HACK()%f[%W]",
+          group = "MiniHipatternsHack",
+        },
+        todo = {
+          pattern = "%f[%w]()TODO()%f[%W]",
+          group = "MiniHipatternsTodo",
+        },
+        note = {
+          pattern = "%f[%w]()NOTE()%f[%W]",
+          group = "MiniHipatternsNote",
+        },
+      },
+    })
   end,
 }
