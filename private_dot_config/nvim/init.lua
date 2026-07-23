b8906dc4abedf2195c23336f96153946048c5b59
Javier Maldonado <3goliad@gmail.com>
2026-07-23 15:40:06 -0400

    enable ruby_lsp

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -79,6 +79,7 @@ vim.lsp.enable("ruff")
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("eslint")
+vim.lsp.enable("ruby_lsp")
 vim.lsp.enable("rubocop")
 
 require("mini.extra").setup()
